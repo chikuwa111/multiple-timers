0f22f940df612157190349dbc37afffc89f6ba64
chikuwa111 <take0shun1111@gmail.com>
2019-11-17 19:04:55 +0900

    Add bottom navigation

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import usePersistedReducer from '../persistedReducer';
 import TimerApp from './TimerApp';
 import ActivityTimeline from './ActivityTimeline';
 import Container from './ui/Container';
+import { PlayIcon, ListBoxIcon } from './ui/Icon';
+import { BottomNavigation, BottomNavItem } from './ui/BottomNavigation';
+
+type App = 'TimerApp' | 'ActivityTimeline';
 
 export default function App() {
   const [state, dispatch] = usePersistedReducer();
+  const [app, setApp] = useState<App>('TimerApp');
 
   return (
-    <Container>
-      <TimerApp state={state} dispatch={dispatch} />
-      <ActivityTimeline state={state} />
-    </Container>
+    <>
+      <Container>
+        {app === 'TimerApp' && <TimerApp state={state} dispatch={dispatch} />}
+        {app === 'ActivityTimeline' && <ActivityTimeline state={state} />}
+      </Container>
+      <BottomNavigation>
+        <BottomNavItem
+          active={app === 'TimerApp'}
+          onClick={() => setApp('TimerApp')}
+        >
+          <PlayIcon />
+        </BottomNavItem>
+        <BottomNavItem
+          active={app === 'ActivityTimeline'}
+          onClick={() => setApp('ActivityTimeline')}
+        >
+          <ListBoxIcon />
+        </BottomNavItem>
+      </BottomNavigation>
+    </>
   );
 }
--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -2,6 +2,6 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   width: 96%;
-  margin: 2rem 2%;
+  margin: 2rem 2% 4rem;
 `;
 export default Container;
