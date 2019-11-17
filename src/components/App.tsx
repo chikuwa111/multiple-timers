e12318c90bf7e9e61ad9643a619691ba9beb58de
chikuwa111 <take0shun1111@gmail.com>
2019-11-18 00:25:28 +0900

    Scroll top when active tab is changed

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import usePersistedReducer from '../persistedReducer';
 import TimerApp from './TimerApp';
 import ActivityTimeline from './ActivityTimeline';
@@ -16,6 +16,10 @@ enum APP {
 export default function App() {
   const [state, dispatch] = usePersistedReducer();
   const [app, setApp] = useState<APP>(APP.TIMER);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [app]);
 
   return (
     <>
