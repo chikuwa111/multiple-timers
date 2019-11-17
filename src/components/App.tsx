0bb03c4815351e3b6aa977f85b09edd3b625be4d
chikuwa111 <take0shun1111@gmail.com>
2019-11-17 15:44:39 +0900

    Update App and mount ActivityTimeline

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,40 +1,12 @@
-import React, { useCallback } from 'react';
-import arrayMove from 'array-move';
-import usePersistedReducer from '../persistedReducer';
-import { updateTimers, add } from '../action';
-import SortableTimerList from './SortableTimerList';
-import Container from './ui/Container';
-import { FullWidthButton } from './ui/Button';
-import { AddIcon } from './ui/Icon';
+import React from 'react';
+import TimerApp from './TimerApp';
+import ActivityTimeline from './ActivityTimeline';
 
 export default function App() {
-  const [state, dispatch] = usePersistedReducer();
-  const { timers } = state;
-
-  const addTimer = useCallback(() => {
-    dispatch(add());
-  }, [dispatch]);
-
-  const onSortEnd = useCallback(
-    ({ oldIndex, newIndex }: { oldIndex: number; newIndex: number }) => {
-      dispatch(updateTimers(arrayMove(timers, oldIndex, newIndex)));
-    },
-    [dispatch, timers]
-  );
-
   return (
-    <Container>
-      <SortableTimerList
-        axis="xy"
-        useDragHandle={true}
-        useWindowAsScrollContainer={true}
-        onSortEnd={onSortEnd}
-        timers={timers}
-        dispatch={dispatch}
-      />
-      <FullWidthButton color="lightcoral" onClick={addTimer}>
-        <AddIcon />
-      </FullWidthButton>
-    </Container>
+    <>
+      <TimerApp />
+      <ActivityTimeline />
+    </>
   );
 }
---
+++ src/components/TimerApp.tsx
@@ -0,0 +1,40 @@
+import React, { useCallback } from 'react';
+import arrayMove from 'array-move';
+import usePersistedReducer from '../persistedReducer';
+import { updateTimers, add } from '../action';
+import SortableTimerList from './SortableTimerList';
+import Container from './ui/Container';
+import { FullWidthButton } from './ui/Button';
+import { AddIcon } from './ui/Icon';
+
+export default function TimerApp() {
+  const [state, dispatch] = usePersistedReducer();
+  const { timers } = state;
+
+  const addTimer = useCallback(() => {
+    dispatch(add());
+  }, [dispatch]);
+
+  const onSortEnd = useCallback(
+    ({ oldIndex, newIndex }: { oldIndex: number; newIndex: number }) => {
+      dispatch(updateTimers(arrayMove(timers, oldIndex, newIndex)));
+    },
+    [dispatch, timers]
+  );
+
+  return (
+    <Container>
+      <SortableTimerList
+        axis="xy"
+        useDragHandle={true}
+        useWindowAsScrollContainer={true}
+        onSortEnd={onSortEnd}
+        timers={timers}
+        dispatch={dispatch}
+      />
+      <FullWidthButton color="lightcoral" onClick={addTimer}>
+        <AddIcon />
+      </FullWidthButton>
+    </Container>
+  );
+}
