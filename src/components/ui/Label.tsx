752b6d4ed838ed470833870931caa7dbae511a09
chikuwa111 <take0shun1111@gmail.com>
2019-11-18 23:51:23 +0900

    Fix Label style

--- src/components/ui/Label.tsx
+++ src/components/ui/Label.tsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 const Label = styled.div`
   display: flex;
   align-items: center;
-  width: 95%;
+  flex-grow: 1;
+  width: 1%;
   padding-left: 2%;
   padding-right: 2%;
 `;
